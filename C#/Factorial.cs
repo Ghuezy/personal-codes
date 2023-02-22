b9a200ebc840b6f2a9fc057da3744b30295d1c56
Salih Dinçay <salihdncay@gmail.com>
2023-02-23 00:52:57 +0300

    Factorial App

---
+++ C#/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorial
+{
+    class  Factorial
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 1, number;
+
+            Console.Write("Enter a number for factorial: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("Factorial is : " + factorial);
+        }
+    }
+}
