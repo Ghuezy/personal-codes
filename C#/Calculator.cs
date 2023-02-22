100729cc50dac744afa9e5dcb9be7f3abad7194a
Salih Dinçay <salihdncay@gmail.com>
2023-02-23 00:43:53 +0300

    Update Calculator.cs

--- C#/Calculator.cs
+++ C#/Calculator.cs
@@ -25,29 +25,29 @@ namespace Calcutor
 
 			switch (opt) {
 
-				case 1:
-					result = num1+ num2;
-					Console.WriteLine(num1 + " + " + num2 + " is : " + result);
-				break;
+			case 1:
+				result = num1+ num2;
+				Console.WriteLine(num1 + " + " + num2 + " is : " + result);
+			break;
 
-                		case 2:
-                    			result = num1 - num2;
-                    			Console.WriteLine(num1 + " - " + num2 + " is : " + result);
-                    		break;
+                	case 2:
+                    		result = num1 - num2;
+                    		Console.WriteLine(num1 + " - " + num2 + " is : " + result);
+                    	break;
 
-                		case 3:
-                    			result = num1 * num2;
-                    			Console.WriteLine(num1 + " * " + num2 + " is : " + result);
-                    		break;
+                	case 3:
+                    		result = num1 * num2;
+                    		Console.WriteLine(num1 + " * " + num2 + " is : " + result);
+                    	break;
 
-                		case 4:
-                    			result = num1 / num2;
-                    			Console.WriteLine(num1 + " / " + num2 + " is : " + result);
-                    		break;
+                	case 4:
+                    		result = num1 / num2;
+                    		Console.WriteLine(num1 + " / " + num2 + " is : " + result);
+                    	break;
 
-				default: 
-					Console.WriteLine("You entered a wrong operator");
-				break;
+			default: 
+				Console.WriteLine("You entered a wrong operator");
+			break;
 					
             		}
 			Console.ReadKey();
