6c424ba23052d5b477debe52e3f171f16d45b0fb
Salih Dinçay <salihdncay@gmail.com>
2023-02-23 00:57:46 +0300

    Calculator App

--- C#/Calculator.cs
+++ C#/Calculator.cs
@@ -3,54 +3,56 @@ using System.Numerics;
 
 namespace Calcutor
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			double num1, num2,result;
-			int opt;
-			Console.Write("Enter the number 1 : ");
-			num1 = Convert.ToInt32(Console.ReadLine());
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result;
+            int opt;
 
-			Console.Write("Enter the number 2 :");
-			num2= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number 1 : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
 
-			Console.WriteLine("Addition > 1: ");
-			Console.WriteLine("Subtraction > 2");
-			Console.WriteLine("Multiplation > 3");
-			Console.WriteLine("Division > 4");
-			Console.Write("Enter the operator : ");
-			opt = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine();
+            Console.Write("Enter the number 2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
 
-			switch (opt) {
+            Console.WriteLine("Addition > 1: ");
+            Console.WriteLine("Subtraction > 2");
+            Console.WriteLine("Multiplation > 3");
+            Console.WriteLine("Division > 4");
+            Console.Write("Enter the operator : ");
+            opt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
 
-			case 1:
-				result = num1+ num2;
-				Console.WriteLine(num1 + " + " + num2 + " is : " + result);
-			break;
+            switch (opt)
+            {
 
-                	case 2:
-                    		result = num1 - num2;
-                    		Console.WriteLine(num1 + " - " + num2 + " is : " + result);
-                    	break;
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine(num1 + " + " + num2 + " is : " + result);
+                    break;
 
-                	case 3:
-                    		result = num1 * num2;
-                    		Console.WriteLine(num1 + " * " + num2 + " is : " + result);
-                    	break;
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine(num1 + " - " + num2 + " is : " + result);
+                    break;
 
-                	case 4:
-                    		result = num1 / num2;
-                    		Console.WriteLine(num1 + " / " + num2 + " is : " + result);
-                    	break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine(num1 + " * " + num2 + " is : " + result);
+                    break;
 
-			default: 
-				Console.WriteLine("You entered a wrong operator");
-			break;
-					
-            		}
-			Console.ReadKey();
-		}
-	}
+                case 4:
+                    result = num1 / num2;
+                    Console.WriteLine(num1 + " / " + num2 + " is : " + result);
+                    break;
+
+                default:
+                    Console.WriteLine("You entered a wrong operator");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
 }
