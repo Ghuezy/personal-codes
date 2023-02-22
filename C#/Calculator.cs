f07f15cebe910237562f0961df6125d8af6ffd4b
Salih Dinçay <salihdncay@gmail.com>
2023-02-23 01:01:00 +0300

    Update Calculator.cs

--- C#/Calculator.cs
+++ C#/Calculator.cs
@@ -1,11 +1,9 @@
-using System;
+﻿using System;
 using System.Numerics;
 
 namespace Calcutor
 {
-
-=======
-	class Program
+	class Calculator
 	{
 		static void Main(string[] args)
 		{
@@ -15,27 +13,23 @@ namespace Calcutor
 			Console.Write("Enter the number 1 : ");
 			num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter the number 1 : ");
-            num1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter the number 2 : ");
+			num2= Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter the number 2 :");
-            num2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Addition > 1: ");
+			Console.WriteLine("Subtraction > 2");
+			Console.WriteLine("Multiplation > 3");
+			Console.WriteLine("Division > 4");
+			Console.Write("Enter the operator : ");
+			opt = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine();
 
-            Console.WriteLine("Addition > 1: ");
-            Console.WriteLine("Subtraction > 2");
-            Console.WriteLine("Multiplation > 3");
-            Console.WriteLine("Division > 4");
-            Console.Write("Enter the operator : ");
-            opt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
+			switch (opt) {
 
-            switch (opt)
-            {
-
-                case 1:
-                    result = num1 + num2;
-                    Console.WriteLine(num1 + " + " + num2 + " is : " + result);
-                    break;
+				case 1:
+				result = num1+ num2;
+				Console.WriteLine(num1 + " + " + num2 + " is : " + result);
+				break;
 
                 case 2:
                     result = num1 - num2;
@@ -52,12 +46,13 @@ namespace Calcutor
                     Console.WriteLine(num1 + " / " + num2 + " is : " + result);
                     break;
 
-                default:
-                    Console.WriteLine("You entered a wrong operator");
-                    break;
-
+				default: 
+					Console.WriteLine("You entered a wrong operator");
+					break;
+					
             }
-            Console.ReadKey();
-        }
-    }
+
+			Console.ReadKey();
+		}
+	}
 }
