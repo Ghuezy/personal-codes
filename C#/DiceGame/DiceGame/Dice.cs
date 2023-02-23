e3682020d79763983643fb2f49a609ca0b2106fc
Salih Dinçay <salihdncay@gmail.com>
2023-02-23 22:33:12 +0300

    Dice Game

---
+++ C#/DiceGame/DiceGame/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarOyunu
+{
+    class Dice
+    {
+
+        int maxValue;
+
+        Random random = new Random();
+
+        public Dice(int maxValue)
+        {
+            this.maxValue = maxValue;
+        }
+
+        public int throwDice()
+        {
+            return random.Next(1, maxValue + 1);
+        }
+    }
+}
---
+++ C#/DiceGame/DiceGame/DiceGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ZarOyunu;
+
+namespace dice
+{
+    class DiceGame
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter max value: ");
+            int maxValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            Dice dice1 = new Dice(maxValue);
+            Dice dice2 = new Dice(maxValue);
+
+            int dice_1 = dice1.throwDice();
+            int dice_2 = dice2.throwDice();
+
+            Console.WriteLine("Dice 1 : " + dice1.throwDice());
+            Console.WriteLine("Dice 2 : " + dice2.throwDice());
+            Console.WriteLine();
+
+            if (dice_1 > dice_2)
+            {
+                Console.WriteLine("Dice 1 is bigger than dice 2.");
+            }
+            else if (dice_1 < dice_2)
+            {
+                Console.WriteLine("Dice 2 is bigger than dice 1.");
+            }
+            else
+            {
+                Console.WriteLine("Dices are equal.");
+            }
+
+
+        }
+    }
+}
